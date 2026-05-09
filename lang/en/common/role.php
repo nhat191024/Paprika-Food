0e4cc2bb647ba705f233fc71d79e8906d2fca91d
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-05-09 19:42:15 +0700

    feat: add language files for role management in Greek, English, and Vietnamese

---
+++ lang/en/common/role.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'admin' => 'Admin',
+    'staff' => 'Staff',
+    'customer' => 'Customer',
+];
---
+++ lang/vi/common/role.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'admin' => 'Quản trị viên',
+    'staff' => 'Nhân viên',
+    'customer' => 'Khách hàng',
+];
