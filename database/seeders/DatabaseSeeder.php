5cd6c07f3f7ebfceb89a8efc9436060030ca0794
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-05-10 12:26:12 +0700

    feat: add OrderSeeder to DatabaseSeeder for order data initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class,
             CategorySeeder::class,
             ProductSeeder::class,
+            OrderSeeder::class,
         ]);
     }
 }
