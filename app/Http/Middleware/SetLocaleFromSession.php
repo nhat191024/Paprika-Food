7f259a4777b14964aa92813cb3c407c17f864bfc
Peredar <64675226+perad4r@users.noreply.github.com>
2026-05-16 15:36:46 +0700

    add: auto set locale

---
+++ app/Http/Middleware/SetLocaleFromSession.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetLocaleFromSession
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = $request->session()->get('locale');
+
+        if (is_string($locale) && $locale !== '') {
+            App::setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
