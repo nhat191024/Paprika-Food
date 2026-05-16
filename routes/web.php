2a1f24b31123b537d90abb9a933284058b056c86
Peredar <64675226+perad4r@users.noreply.github.com>
2026-05-16 15:51:13 +0700

    update: controller for swithing locales

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\LocaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+
+Route::post('locale', LocaleController::class)->name('locale.update');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
+require __DIR__.'/auth.php';
 require __DIR__.'/settings.php';
