0b6a96a8ecc1e47b48c32509fb7e088e98e81dee
Peredar <64675226+perad4r@users.noreply.github.com>
2026-05-16 15:55:04 +0700

    add: component for lang switching and popups

---
+++ resources/views/components/language-switcher.blade.php
@@ -0,0 +1,30 @@
+@php
+$availableLocales = collect(\Illuminate\Support\Facades\File::directories(lang_path()))
+    ->map(fn (string $directory): string => basename($directory))
+    ->reject(fn (string $locale): bool => $locale === 'vendor')
+    ->sort()
+    ->values();
+
+$localeLabels = [
+    'el' => 'Greek',
+    'en' => 'English',
+    'vi' => 'Tiếng Việt',
+];
+@endphp
+
+<flux:menu.submenu :heading="__('common/language.switch')" icon="language">
+    @foreach ($availableLocales as $locale)
+        <form method="POST" action="{{ route('locale.update') }}" class="w-full">
+            @csrf
+            <input type="hidden" name="locale" value="{{ $locale }}">
+            <flux:menu.item
+                as="button"
+                type="submit"
+                class="w-full cursor-pointer"
+                :icon="app()->getLocale() === $locale ? 'check' : null"
+            >
+                {{ $localeLabels[$locale] ?? strtoupper($locale) }}
+            </flux:menu.item>
+        </form>
+    @endforeach
+</flux:menu.submenu>
